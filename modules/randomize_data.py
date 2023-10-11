3fc193d7ee9df4dd93d3fd8d0c6d1ec4d9c94392
Konstantin-create <schahov.konst@gmail.com>
2023-10-11 23:17:45 +0300

    edit: carriages random generation

--- modules/randomize_data.py
+++ modules/randomize_data.py
@@ -5,11 +5,11 @@ from modules.classes import *
 
 class RandomData:
     def __init__(self):
-        pass
+        self.carriages_types = [SeatCarriage, EconomCarriage, CoupeCarriage, FirstClassCarriage]
+        self.carriages_weights = [0.25, 0.3, 0.35, 0.1]
 
     def generate_routes(self) -> list:
         routes = []
-        carriages_types = random.choice([SeatCarriage, EconomCarriage, CoupeCarriage, FirstClassCarriage])
 
         for i in range(random.randint(Config.TRAIN_PER_ROUTE_MIN_QUANTITY, Config.TRAIN_PER_ROUTE_MAX_QUANTITY)):
             routes.append(
@@ -19,6 +19,12 @@ class RandomData:
                     time=f'{random.randint(0, 23):02}:{random.randint(0, 59):02}'
                 )
             )
+
+            for j in range(random.randint(Config.CARRIAGE_MIN_QUANTITY, Config.CARRIAGE_MAX_QUANTITY)):
+                carriage_type = random.choices(self.carriages_types, self.carriages_weights, k=1)
+                routes[-1].train.carriages.append(
+
+                )
 
         routes.sort(key=lambda x: (int(x.time.split(':')[0]), int(x.time.split(':')[1])))
         return routes
