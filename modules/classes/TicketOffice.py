7f2745f808b8404f9b83c98120e9074473fe13e2
Konstantin-create <schahov.konst@gmail.com>
2023-10-12 23:14:23 +0300

    feat: working on tui: first screen complite

--- modules/classes/TicketOffice.py
+++ modules/classes/TicketOffice.py
@@ -6,17 +6,17 @@ class TicketOffice(npyscreen.NPSAppManaged):
         self.addForm("MAIN", WelcomeForm)
 
 
-class WelcomeForm(npyscreen.Form):
+class WelcomeForm(npyscreen.ActionForm):
     def create(self):
         center_x = int((self.max_x - len("Текст по центру")) / 2)
         open('out.txt', 'w').write(str([self.max_x, center_x]))
         self.add(npyscreen.FixedText, value="Текст по центру", editable=False, relx=center_x)
-        self.add(npyscreen.ButtonPress, name="Закрыть", when_pressed_function=self.close_app, relx=self.max_x - 30,
-                 rely=self.max_y - 10)
-        self.add(npyscreen.ButtonPress, name="Далее", when_pressed_function=self.dummy_func)
+        self.OK_BUTTON_TEXT = 'Далее'
+        self.CANCEL_BUTTON_TEXT = 'Закрыть'
 
-    def dummy_func(self):
-        pass
+    def on_ok(self):
+        print('Next page')
+        self.parentApp.setNextForm(None)
 
-    def close_app(self):
-        self.parentApp.switchForm(None)
+    def on_cancel(self):
+        self.parentApp.setNextForm(None)
