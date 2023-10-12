a3f4d74e26eaebc5674782533d73dc80b6f05878
Konstantin-create <schahov.konst@gmail.com>
2023-10-12 23:01:40 +0300

    feat: working on tui

--- modules/classes/TicketOffice.py
+++ modules/classes/TicketOffice.py
@@ -1,19 +1,22 @@
 import npyscreen
 
 
-class TicketOffice(npyscreen.StandardApp):
+class TicketOffice(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm("MAIN", WelcomeForm)
 
 
 class WelcomeForm(npyscreen.Form):
-    # Конструктор
     def create(self):
-        cols, _ = self.useable_space()
-        title_widget = self.add(npyscreen.TitleText, name="Добро пожаловать в систему покупки билетов!", rely=1, editable=False)
-        # title_widget.relx = int((cols - len(title_widget.value)) / 2)
+        center_x = int((self.max_x - len("Текст по центру")) / 2)
+        open('out.txt', 'w').write(str([self.max_x, center_x]))
+        self.add(npyscreen.FixedText, value="Текст по центру", editable=False, relx=center_x)
+        self.add(npyscreen.ButtonPress, name="Закрыть", when_pressed_function=self.close_app, relx=self.max_x - 30,
+                 rely=self.max_y - 10)
+        self.add(npyscreen.ButtonPress, name="Далее", when_pressed_function=self.dummy_func)
 
-    # переопределенный метод, срабатывающий при нажатии на кнопку «ok»
-    def on_ok(self):
-        self.parentApp.
+    def dummy_func(self):
+        pass
 
+    def close_app(self):
+        self.parentApp.switchForm(None)
