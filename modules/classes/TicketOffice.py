0bdf867057ab22be006d3779f526979cea063376
Konstantin-create <schahov.konst@gmail.com>
2023-10-18 22:17:51 +0300

    feat: working on tui - fix bugs while changing formes

--- modules/classes/TicketOffice.py
+++ modules/classes/TicketOffice.py
@@ -80,33 +80,47 @@ class ChoseRoute(npyscreen.Form):
             self.parentApp.setNextForm('SECOND')
         else:
             global_route_id = int(self.get_id.value.strip())
+
+        if len(list(filter(lambda x: x.id == global_route_id, rand_data))) == 0:
+            npyscreen.notify_confirm(message=f"Произошла ошибка! Такого маршрута не существует, проверьте ввод",
+                                     title="Ошибка!")
+            self.parentApp.setNextForm('SECOND')
+        else:
             self.parentApp.setNextForm('THIRD')
 
 
 class ChoseCarriage(npyscreen.Form):
     def create(self):
         self.title = self.add(npyscreen.FixedText, value='Выберите тип вагона:', editable=False)
-        self.route_id = self.add(npyscreen.FixedText, value=f'Номер отправления: {global_route_id}', editable=False)
+        self.route_id = self.add(npyscreen.FixedText, value=f'Номер отправления: {global_route_id}', editable=False,
+                                 rely=5)
         self.route_time = self.add(npyscreen.FixedText, value=f'Время отправления: {None}', editable=False)
         self.route_carriage_title = self.add(npyscreen.FixedText, value='Билеты:', editable=False)
         self.carriage_type1 = self.add(npyscreen.FixedText, editable=False)
         self.carriage_type2 = self.add(npyscreen.FixedText, editable=False)
         self.carriage_type3 = self.add(npyscreen.FixedText, editable=False)
         self.carriage_type4 = self.add(npyscreen.FixedText, editable=False)
+        self.id_type = self.add(NumericInput, name="Введите тип вагона:", value="", editable=True, rely=50)
 
     def beforeEditing(self):
-        route = list(filter(lambda x: x.id == global_route_id, rand_data))
-        if route:
-            route = route[0]
-        else:
-            pass
-
+        route = list(filter(lambda x: x.id == global_route_id, rand_data))[0]
         self.route_id.value = f'Номер отправления: {global_route_id}'
+
         self.route_time.value = f'Время отправления: {route.time}'
         self.carriage_type1.value = f'1 - СИДЯЧИЕ: {route.train.count_free_seats(carriage_type=SeatCarriage)}/{SeatCarriage().max_seats_quantity}'
         self.carriage_type2.value = f'2 - ПЛАЦКАРТ: {route.train.count_free_seats(carriage_type=EconomCarriage)}/{EconomCarriage().max_seats_quantity}'
         self.carriage_type3.value = f'3 - КУПЕ: {route.train.count_free_seats(carriage_type=CoupeCarriage)}/{CoupeCarriage().max_seats_quantity}'
         self.carriage_type4.value = f'4 - СВ: {route.train.count_free_seats(carriage_type=FirstClassCarriage)}/{FirstClassCarriage().max_seats_quantity}'
 
+    def check_input(self):
+        value: str = self.id_type.value.strip()
+        return value.isdigit()
+
     def afterEditing(self):
-        self.parentApp.setNextForm(None)
+        global global_route_id
+        if not self.check_input():
+            npyscreen.notify_confirm(message=f"Произошла ошибка! Тип вагона должен быть числом, проверьте ввод",
+                                     title="Ошибка!")
+            self.parentApp.setNextForm(None)
+        else:
+            self.parentApp.setNextForm(None)
