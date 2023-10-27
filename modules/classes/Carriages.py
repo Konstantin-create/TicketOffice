8b67d8b568dbaa31993f2b2aa706aa6eeb811ea6
Konstantin-create <schahov.konst@gmail.com>
2023-10-27 21:53:43 +0300

    create classes: Route, Train, Carriages

---
+++ modules/classes/Carriages.py
@@ -0,0 +1,72 @@
+from modules.tools import count_free_seats
+
+
+class Carriage:
+    def __init__(self, name: str = None, price: int = -1, max_seats_quantity: int = -1, seats=None):
+        if seats is None:
+            seats = []
+
+        self.name = name
+        self.price = price
+        self.max_seats_quantity = max_seats_quantity
+        self.seats = seats
+
+    def __repr__(self):
+        return f'<Carriage: {self.name} | {self.price} | {count_free_seats(self.seats)}/{self.max_seats_quantity}>'
+
+
+class SeatCarriage(Carriage):
+    def __init__(self, seats: list = None):
+        super().__init__()
+        if seats is None:
+            seats = []
+
+        self.name = 'сидячий вагон'
+        self.price = 500
+        self.max_seats_quantity = 60
+        self.seats = seats
+
+
+class EconomCarriage(Carriage):
+    def __init__(self, seats: list = None):
+        super().__init__()
+        if seats is None:
+            seats = []
+
+        self.name = 'вагон эконом'
+        self.price = 1000
+        self.max_seats_quantity = 60
+        self.seats = seats
+
+
+class CoupeCarriage(Carriage):
+    def __init__(self, seats: list = None):
+        super().__init__()
+        if seats is None:
+            seats = []
+
+        self.name = 'вагон купе'
+        self.price = 2000
+        self.max_seats_quantity = 20
+        self.seats = seats
+
+
+class FirstClassCarriage(Carriage):
+    def __init__(self, seats: list = None):
+        super().__init__()
+        if seats is None:
+            seats = []
+
+        self.name = 'вагон св'
+        self.price = 5000
+        self.max_seats_quantity = 10
+        self.seats = seats
+
+
+class Seat:
+    def __init__(self, id: int = -1, is_busy: bool = False):
+        self.id = id
+        self.is_busy = is_busy
+
+    def __repr__(self):
+        return f'<Seat: {self.id} | busy? - {self.is_busy}>'
---
+++ modules/classes/Route.py
@@ -0,0 +1,11 @@
+from modules.classes.Train import Train
+
+
+class Route:
+    def __init__(self, id: int = -1, train: Train | None = None, time: str = ''):
+        self.id = id
+        self.train = train
+        self.time = time  # todo: rewrite on datetime obj
+
+    def __repr__(self):
+        return f'<Route: {self.id} | {self.train} | {self.time}>'
